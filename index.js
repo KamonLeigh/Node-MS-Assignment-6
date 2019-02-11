f824b9aa90e3af585ec3bd98ec18c39eabca16aa
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2019-02-11 23:25:53 +0000

    Set server.js and added SSL file for https

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ *  This assignment no 6 for the NODE Masterclass
+ *  Create simple API route /hello with welcome message 
+ *  Add cluster module to application
+ * 
+ * 
+ *  author: Byron Dunkley
+ * 
+ * 
+ * 
+ */
